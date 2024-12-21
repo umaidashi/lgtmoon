f8b28c6fdf00610fbddc2f9d67ccbfa6550a6933
umaidashi <umaidashi18@gmail.com>
2024-12-21 16:35:29 +0900

    feat: add hooks

---
+++ src/hooks.ts
@@ -0,0 +1,28 @@
+import { SearchResult, Image } from "./type";
+import { useFetch } from "@raycast/utils";
+
+export const useFetchLgtmoonRecent = (): { isLoading: boolean, data: Image[], revalidate: () => void } => {
+  const { isLoading, data, revalidate } = useFetch("https://lgtmoon.dev/api/images/recent", {
+    mapResult(result: SearchResult) {
+      return {
+        data: result.images
+      }
+    },
+    initialData: []
+  });
+
+  return { isLoading, data, revalidate };
+}
+
+export const useFetchLgtmoonRandom = (): { isLoading: boolean, data: Image[], revalidate: () => void } => {
+  const { isLoading, data, revalidate } = useFetch("https://lgtmoon.dev/api/images/random", {
+    mapResult(result: SearchResult) {
+      return {
+        data: result.images
+      }
+    },
+    initialData: []
+  });
+
+  return { isLoading, data, revalidate };
+}
