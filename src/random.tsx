4da95e02896db5b0c205005ab761e019de7ecdf3
umaidashi <umaidashi18@gmail.com>
2024-12-21 16:36:03 +0900

    feat: add random command

---
+++ src/random.tsx
@@ -0,0 +1,32 @@
+import { ActionPanel, Action, Grid } from "@raycast/api";
+import { useFetchLgtmoonRandom } from "./hooks";
+import { Actions } from "./components";
+
+export default function Command() {
+  const { isLoading, data, revalidate } = useFetchLgtmoonRandom();
+
+  return (
+    <Grid
+      isLoading={isLoading}
+      actions={
+        <ActionPanel>
+          <Action title="Select" onAction={() => revalidate()} />
+        </ActionPanel>
+      }
+    >
+      {
+        data.map((image) => {
+          const key = image.url.split("/").slice(-1)[0];
+
+          return (
+            <Grid.Item
+              key={key}
+              content={image.url}
+              title={key}
+              actions={<Actions url={image.url} />}
+            />
+          )
+        })}
+    </Grid>
+  );
+}
