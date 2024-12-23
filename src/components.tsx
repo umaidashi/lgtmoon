fba8e6d3011a52f808e1e2cfcb58bb3e198daa8b
umaidashi <umaidashi18@gmail.com>
2024-12-23 17:24:34 +0900

    fix: paste url

--- src/components.tsx
+++ src/components.tsx
@@ -1,9 +1,11 @@
 import { Action, ActionPanel } from "@raycast/api";
 
 export const Actions = ({ url }: { url: string }) => {
+  const urlMarkdown = `[${url}](${url})`;
   return (
     <ActionPanel>
-      <Action.Paste content={url} title="Paste Lgtm" />
+      <Action.Paste content={urlMarkdown} title="Paste Lgtm" />
+      <Action.CopyToClipboard content={urlMarkdown} title="Copy Lgtm" />
       <Action.OpenInBrowser url={url} title="Open in Browser" />
       <Action.OpenInBrowser
         url={"https://github.com/pulls?q=sort:updated-desc+is:pr+is:open+archived:false+review-requested:@me+"}
