b0c0088f7b0a18e044970275c3227c1f47a1ea9d
umaidashi <umaidashi18@gmail.com>
2025-01-13 12:26:33 +0900

    refactor: improve formatting and readability of Lgtmoon fetcher function

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,18 +1,19 @@
 import { SearchResult, Image } from "./type";
 import { useFetch } from "@raycast/utils";
 
-const createLgtmoonFetcher = (endpoint: string) => (): { isLoading: boolean; data: Image[]; revalidate: () => void } => {
-  const { isLoading, data, revalidate } = useFetch(`https://lgtmoon.dev/api/images/${endpoint}`, {
-    mapResult(result: SearchResult) {
-      return {
-        data: result.images,
-      };
-    },
-    initialData: [],
-  });
+const createLgtmoonFetcher =
+  (endpoint: string) => (): { isLoading: boolean; data: Image[]; revalidate: () => void } => {
+    const { isLoading, data, revalidate } = useFetch(`https://lgtmoon.dev/api/images/${endpoint}`, {
+      mapResult(result: SearchResult) {
+        return {
+          data: result.images,
+        };
+      },
+      initialData: [],
+    });
 
-  return { isLoading, data, revalidate };
-};
+    return { isLoading, data, revalidate };
+  };
 
 export const useFetchLgtmoonRecent = createLgtmoonFetcher("recent");
 export const useFetchLgtmoonRandom = createLgtmoonFetcher("random");
